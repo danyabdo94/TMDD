67a0717f699129983d018481c13806cfeb1848f2
danyabdo94 <danyabdo94@gmail.com>
2021-06-13 01:02:13 +0200

    Add number styles

--- src/components/movie.tsx
+++ src/components/movie.tsx
@@ -15,21 +15,26 @@ function Movie({ movie }: { movie: any }) {
         </div>
       </div>
       <div className="w-full pt-6 pr-2 pb-3 pl-2 relative flex content-start flex-wrap">
-        <div className="absolute -top-5 left-3 w-8 h-8 box-border">
-          <div className="rounded-full w-8 h-8 ">
+        <div className="absolute -top-3 left-3 w-9 max-h-9 box-border inline-block scale-100">
+          <div className="rounded-full w-9 max-h-9 mr-0 inline-block bg-black top-0">
             <div
-              className="user_score_chart 553c3366c3a3683e5500460a"
+              className="relative w-full max-h-9 text-center"
               data-percent="87.0"
               data-track-color="#204529"
               data-bar-color="#21d07a"
             >
-              <div className="percent">
-                <span className="icon icon-r87"></span>
+              <div className="w-full max-h-9 flex z-10 justify-center items-center">
+                <span
+                  className={`text-white p-2 icon icon-r${movie.vote_average * 10}`}
+                  style={{
+                   
+                  }}
+                ></span>
               </div>
               <canvas
-                height="68"
-                width="68"
-                style={{height: "34", width: "34"}}
+                height="34"
+                width="34"
+                style={{ height: "34", width: "34" }}
               ></canvas>
             </div>
           </div>
--- src/features/movies-list/movies-list.tsx
+++ src/features/movies-list/movies-list.tsx
@@ -17,12 +17,12 @@ function MoviesList() {
   return (
     <main className="w-full mt-24 sm:mt-16 min-h-full px-10 py-8">
       <h2 className="text-2xl font-semibold mb-5">Popular Movies</h2>
-      <div className="w-full flex">
-        <div className="min-w-1/5 w-1/5">
+      <div className="w-full flex flex-wrap">
+        <div className="w-full sm:min-w-1/5 sm:w-1/5">
           <Filters />
         </div>
-        <div className="pl-8 min-w-4/5 w-4/5">
-          <div className="w-full flex justify-between flex-wrap -mt-7">
+        <div className="pl-8 w-full sm:min-w-4/5 sm:w-4/5">
+          <div className="w-full flex justify-between flex-wrap sm:-mt-7">
             {data?.results.map((element) => (
               <Movie movie={element}></Movie>
             ))}
